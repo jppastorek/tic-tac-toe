b21c905ba7b26388749e63509f5dd27f5a74d927
jppastorek <jppastorek@gmail.com>
2022-02-05 09:38:46 -0600

    Finished new game features.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This is part of The Odin Project's JavaScript path. I am tasked with creating this game using objects, but I'm not fully certain I nailed it. I know that at least one of these should be anonymous (called immediately) but I can't remember how to do that, and I don't want to break it.
 The logic for determining a winner isn't the most sophisticated, but it works. I definitely want to refactor it to loop through the indices of the board using an array of Winning Combos (the index positions at which X or O must be present for a win event), but that will happen at a later date.
+
+Update 2/5/22
+
+Added all functionality. I couldn't figure out how to avoid using global variables. I do see the point of anonymous functions that would be declared immediately, but I guess I need some help to figure out how to use those better and to avoid the globals.
--- scripts.js
+++ scripts.js
@@ -2,6 +2,42 @@ const createPlayer = (name, marker, active) => ({ name, marker, active })
 
 const player1 = createPlayer('player1', 'x', true);
 const player2 = createPlayer('player2', 'o', false);
+const buttonPlay = document.querySelector(".button-play");
+const buttonNewPlayers = document.querySelector(".button-new");
+const gameContainer = document.querySelector(".game-container");
+const button1 = document.querySelector(".button1");
+const input1 = document.querySelector("#player1name");
+const modal1 = document.querySelector(".modal1");
+const modal2 = document.querySelector(".modal2");
+const button2 = document.querySelector(".button2");
+const input2 = document.querySelector("#player2name");
+
+
+button2.addEventListener("click", () => {
+    player2.name = input2.value;
+    modal2.classList.add("hidden");
+});
+
+button1.addEventListener("click", () => {
+    player1.name = input1.value;
+    modal1.classList.add("hidden");
+    modal2.classList.remove("hidden");
+});
+
+buttonPlay.addEventListener("click", () => {
+    displayController.newGame();
+    displayController.displayMarker();
+    player1.active = true;
+    player2.active = false;
+});
+
+buttonNewPlayers.addEventListener("click", () => {
+    displayController.newGame();
+    displayController.displayMarker();
+    player1.active = true;
+    player2.active = false;
+    displayController.setPlayer1Name();
+});
 
 const gameBoard = {
     board: [],
@@ -14,7 +50,6 @@ const gameBoard = {
             this.board.push(div);
             index++;
         }
-        const gameContainer = document.querySelector(".game-container");
         this.board.forEach((square) => {
             gameContainer.appendChild(square);
         });
@@ -47,7 +82,18 @@ const displayController = {
         });
     },
     checkForWinner: function() {
+        const modal3 = document.querySelector(".modal3");
+        const winnerText = document.querySelector("#winner");
         let currentState = [];
+        let winningMarker = "";
+
+        function declareWinner() {
+            if (winningMarker === "X") {
+                winnerText.innerText = `${player1.name} wins!`;
+            } else if (winningMarker === "O") {
+                winnerText.innerText = `${player2.name} wins!`;
+            }
+        };
         gameBoard.board.forEach((square) => {
             currentState.push(square.innerText);
         });
@@ -58,99 +104,138 @@ const displayController = {
         row2 = row2.toString();
         row3 = row3.toString();
         if (row1 === "X,X,X" || row2 === "X,X,X" || row3 === "X,X,X") {
+            winningMarker = "X";
             if (row1 === "X,X,X") {
                 gameBoard.board[0].classList.add("winner");
                 gameBoard.board[1].classList.add("winner");
                 gameBoard.board[2].classList.add("winner");
+                modal3.classList.remove("hidden");
+                declareWinner();
             } else if (row2 === "X,X,X") {
                 gameBoard.board[3].classList.add("winner");
                 gameBoard.board[4].classList.add("winner");
                 gameBoard.board[5].classList.add("winner");
+                modal3.classList.remove("hidden");
+                declareWinner();
             } else if (row3 === "X,X,X") {
                 gameBoard.board[6].classList.add("winner");
                 gameBoard.board[7].classList.add("winner");
                 gameBoard.board[8].classList.add("winner");
+                modal3.classList.remove("hidden");
+                declareWinner();
             }
         } else if (row1 === "O,O,O" || row2 === "O,O,O" || row3 === "O,O,O") {
+            winningMarker = "O";
             if (row1 === "O,O,O") {
                 gameBoard.board[0].classList.add("winner");
                 gameBoard.board[1].classList.add("winner");
                 gameBoard.board[2].classList.add("winner");
+                modal3.classList.remove("hidden");
+                declareWinner();
             } else if (row2 === "O,O,O") {
                 gameBoard.board[3].classList.add("winner");
                 gameBoard.board[4].classList.add("winner");
                 gameBoard.board[5].classList.add("winner");
+                modal3.classList.remove("hidden");
+                declareWinner();
             } else if (row3 === "O,O,O") {
                 gameBoard.board[6].classList.add("winner");
                 gameBoard.board[7].classList.add("winner");
                 gameBoard.board[8].classList.add("winner");
+                modal3.classList.remove("hidden");
+                declareWinner();
             }
         } else if (gameBoard.board[0].innerText === "X" && gameBoard.board[3].innerText === "X" && gameBoard.board[6].innerText === "X") { //column 1 good
+            winningMarker = "X";
             gameBoard.board[0].classList.add("winner");
             gameBoard.board[3].classList.add("winner");
             gameBoard.board[6].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[1].innerText === "X" && gameBoard.board[4].innerText === "X" && gameBoard.board[7].innerText === "X") { //column 2 BAD
+            winningMarker = "X";
             gameBoard.board[1].classList.add("winner");
             gameBoard.board[4].classList.add("winner");
             gameBoard.board[7].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[2].innerText === "X" && gameBoard.board[5].innerText === "X" && gameBoard.board[8].innerText === "X") { //column 3
+            winningMarker = "X";
             gameBoard.board[2].classList.add("winner");
             gameBoard.board[5].classList.add("winner");
             gameBoard.board[8].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[0].innerText === "X" && gameBoard.board[4].innerText === "X" && gameBoard.board[8].innerText === "X") {
+            winningMarker = "X";
             gameBoard.board[0].classList.add("winner");
             gameBoard.board[4].classList.add("winner");
             gameBoard.board[8].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[2].innerText === "X" && gameBoard.board[4].innerText === "X" && gameBoard.board[6].innerText === "X") {
+            winningMarker = "X";
             gameBoard.board[2].classList.add("winner");
             gameBoard.board[4].classList.add("winner");
             gameBoard.board[6].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[0].innerText === "O" && gameBoard.board[3].innerText === "O" && gameBoard.board[6].innerText === "O") { //column 1 good
+            winningMarker = "O";
             gameBoard.board[0].classList.add("winner");
             gameBoard.board[3].classList.add("winner");
             gameBoard.board[6].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[1].innerText === "O" && gameBoard.board[4].innerText === "O" && gameBoard.board[7].innerText === "O") { //column 2 BAD
+            winningMarker = "O";
             gameBoard.board[1].classList.add("winner");
             gameBoard.board[4].classList.add("winner");
             gameBoard.board[7].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[2].innerText === "O" && gameBoard.board[5].innerText === "O" && gameBoard.board[8].innerText === "O") { //column 3
+            winningMarker = "O";
             gameBoard.board[2].classList.add("winner");
             gameBoard.board[5].classList.add("winner");
             gameBoard.board[8].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[0].innerText === "O" && gameBoard.board[4].innerText === "O" && gameBoard.board[8].innerText === "O") {
+            winningMarker = "O";
             gameBoard.board[0].classList.add("winner");
             gameBoard.board[4].classList.add("winner");
             gameBoard.board[8].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         } else if (gameBoard.board[2].innerText === "O" && gameBoard.board[4].innerText === "O" && gameBoard.board[6].innerText === "O") {
+            winningMarker = "O";
             gameBoard.board[2].classList.add("winner");
             gameBoard.board[4].classList.add("winner");
             gameBoard.board[6].classList.add("winner");
+            modal3.classList.remove("hidden");
+            declareWinner();
         }
     },
     setPlayer1Name: function() {
-        const button1 = document.querySelector(".button1");
-        const input1 = document.querySelector("#player1name");
-        const modal1 = document.querySelector(".modal1");
-        const modal2 = document.querySelector(".modal2");
-        button1.addEventListener("click", () => {
-            player1.name = input1.value;
-            modal1.classList.add("hidden");
-            modal2.classList.remove("hidden");
-        })
+        modal1.classList.remove("hidden");
     },
-    setPlayer2Name: function() {
-        const button2 = document.querySelector(".button2");
-        const input2 = document.querySelector("#player2name");
-        const modal2 = document.querySelector(".modal2");
-        button2.addEventListener("click", () => {
-            player2.name = input2.value;
-            modal2.classList.add("hidden");
+    newGame: function() {
+        const modal3 = document.querySelector(".modal3");
+        modal3.classList.add("hidden");
+        gameContainer.innerHTML = "";
+        gameBoard.board = [];
+        gameBoard.create();
+        gameBoard.board.forEach((cell) => {
+            cell.innerText = "";
+            if (cell.classList.contains("winner")) {
+                cell.classList.remove("winner");
+            }
         })
     },
 }
 
 
 displayController.setPlayer1Name();
-displayController.setPlayer2Name();
 
 displayController.displayMarker();
